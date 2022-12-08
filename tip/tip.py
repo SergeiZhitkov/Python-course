52825531e84cd41cfb2bac657b4ae4ff622c61a9
SergeiZhitkov <104830374+SergeiZhitkov@users.noreply.github.com>
2022-12-08 09:00:44 +0000

    Thu, Dec 8, 2022, 9:00 AM +00:00

--- tip/tip.py
+++ tip/tip.py
@@ -6,11 +6,15 @@ def main():
 
 
 def dollars_to_float(d):
-    
+    d = d.replace("$", "")
+    d = float(d)
+    return f"{d:.1f}"
 
 
 def percent_to_float(p):
-
+    p = p.replace("%", "")
+    p = float(p) / 100
+    return f"{p:.2f}"
 
 if __name__ == "__main__":
     main()
