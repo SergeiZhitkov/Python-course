0921bab39a68782b02fdb261662a03ad68ff3c55
SergeiZhitkov <104830374+SergeiZhitkov@users.noreply.github.com>
2022-12-08 08:46:31 +0000

    Thu, Dec 8, 2022, 8:46 AM +00:00

---
+++ faces/faces.py
@@ -0,0 +1,19 @@
+def main():
+    message = input()
+    print(convert(message))
+
+
+
+
+
+
+def convert():
+    message = message.replace(":)","🙂")
+    message = message.replace(":(", "🙁")
+
+
+
+
+
+if __name__ == "__main__":
+    main()
