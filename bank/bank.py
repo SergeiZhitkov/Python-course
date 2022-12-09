c3c551055d646ad0129d5672f096cead1a0b1972
SergeiZhitkov <104830374+SergeiZhitkov@users.noreply.github.com>
2022-12-09 04:59:23 +0000

    Fri, Dec 9, 2022, 4:59 AM +00:00

--- bank/bank.py
+++ bank/bank.py
@@ -7,9 +7,14 @@ def main():
 def dollar_gives(g):
     words = g.split(" ")
     p = ""
-    for letter in words[0]:
-        p = p + letter
-    if p == "hello"
+    for word in words:
+        for letter in word[0-5]:
+            p = p + letter
+    if p == "hello":
+        return "$0"
+    elif p[0] == "h":
+        return "$20"
+    return "$100"
 
 
 if __name__ == "__main__":
