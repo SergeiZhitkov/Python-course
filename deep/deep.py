3f2d2276d0db33eeec701376b06bc3c09ec1fefc
SergeiZhitkov <104830374+SergeiZhitkov@users.noreply.github.com>
2022-12-09 04:34:27 +0000

    Fri, Dec 9, 2022, 4:34 AM +00:00

---
+++ deep/deep.py
@@ -0,0 +1,19 @@
+def main():
+    answer = input("What is the Answer to the Great Question of Life, The Universe and Everything? ").lower()
+    print(is_right(answer))
+
+
+
+def is_right(a):
+    if a == "42" or a == "forty-two" or a == "forty two":
+        return "Yes"
+    else:
+        return "No"
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
