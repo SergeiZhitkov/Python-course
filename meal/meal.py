109f06886882e10a9a2a81a16e1f59a6df187f49
SergeiZhitkov <104830374+SergeiZhitkov@users.noreply.github.com>
2022-12-09 10:33:57 +0000

    Fri, Dec 9, 2022, 10:33 AM +00:00

--- meal/meal.py
+++ meal/meal.py
@@ -1,10 +1,19 @@
 def main():
-    ...
+    time = input("What's time? ").strip()
+    print(convert(time))
 
-
-def convert(time):
-    ...
-
+def convert(t):
+    hour, minut = t.split(":")
+    hour = float(hour)
+    minut = float(minut) / 60
+    t = hour + minut
+    if 7 <= t <= 8:
+        return "breakfast time"
+    elif 12 <= t <= 13:
+        return "lunch time"
+    elif 18 <= t <= 19:
+        return "dinner time"
+    
 
 if __name__ == "__main__":
     main()
