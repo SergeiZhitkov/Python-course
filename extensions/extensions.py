b4c4cbf6a6a7048a565461492e1baa24e5daa7c9
SergeiZhitkov <104830374+SergeiZhitkov@users.noreply.github.com>
2022-12-09 05:42:26 +0000

    Fri, Dec 9, 2022, 5:42 AM +00:00

--- extensions/extensions.py
+++ extensions/extensions.py
@@ -1,3 +1,5 @@
+
+
 def main():
     file = input("File name: ")
     print(file_extension(file))
@@ -5,9 +7,17 @@ def main():
 
 
 def file_extension(f):
-    
-
-
+    extensions = {
+        ".gif" : "image/gif",
+        ".jpg" : "image/jpeg",
+        ".jpeg" : "image/jpeg",
+        ".png" : "image/png",
+        ".pdf" : "application/pdf",
+        ".txt" : "text/plain",
+        ".zip" : "application/zip",
+       }
+    if f.endswith(".gif", ".jpg", ".jpeg", ".png", ".pdf", ".txt", ".zip"):
+        
 
 
 
