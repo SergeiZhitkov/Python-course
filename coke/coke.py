331d6982c82c861428672444d4efa74b915ef3d6
SergeiZhitkov <104830374+SergeiZhitkov@users.noreply.github.com>
2022-12-10 04:41:38 +0000

    Sat, Dec 10, 2022, 4:41 AM +00:00

---
+++ coke/coke.py
@@ -0,0 +1,23 @@
+def main():
+    cost = 50
+    while True:
+        print(f"Amount Due: {cost}")
+        coin = int(input("Insert Coin: "))
+        if check_coin(coin):
+            cost = cost - coin
+
+
+
+def check_coin(c):
+    allowed_values = [5, 10, 25]
+    if c in allowed_values:
+        return True
+    return False
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
