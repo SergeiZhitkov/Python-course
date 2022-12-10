db8ed23889753ec1ea5525f6bffddc8698ea51c7
SergeiZhitkov <104830374+SergeiZhitkov@users.noreply.github.com>
2022-12-10 05:16:05 +0000

    Sat, Dec 10, 2022, 5:16 AM +00:00

--- plates/plates.py
+++ plates/plates.py
@@ -7,11 +7,14 @@ def main():
 
 
 def is_valid(s):
+    i = 0
     if 1 < len(s) < 7:
         for letter in s[1:]:
-            if letter.isalpha() and s .isalpha():
+            if letter.isalpha() and s[i].isalpha():
+                i += 1
                 continue
             elif letter.isnumeric():
+                i += 1
                 continue
             else:
                 return False
