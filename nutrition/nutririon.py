55006d5aa9236bb2945b02233398597b4781f61e
SergeiZhitkov <104830374+SergeiZhitkov@users.noreply.github.com>
2022-12-10 11:24:04 +0000

    Sat, Dec 10, 2022, 11:24 AM +00:00

--- nutrition/nutririon.py
+++ nutrition/nutririon.py
@@ -1,3 +1,4 @@
+import sys
 def main():
     fruit = input("Item: ").strip().lower()
     print(calorie_cal(fruit))
@@ -7,15 +8,31 @@ def main():
 
 def calorie_cal(f):
     rfl = {
-        "apple" : 
-
-
-
-
-
-
+        "apple" : 130,
+        "avocado california" : 50,
+        "banana" : 110,
+        "cantaloupe" : 50,
+        "grapefruit" : 60,
+        "grapes" : 90,
+        "honeydew melon" : 50,
+        "kiwifruit" : 90,
+        "lemon" : 15,
+        "lime" : 20,
+        "nectarine" : 60,
+        "orange" : 80,
+        "peach" : 60,
+        "pear" : 100,
+        "pineapple" : 50,
+        "plums" : 70,
+        "strawberries" : 50,
+        "sweet cherries" : 100,
+        "tangerine" : 50,
+        "watermelon" : 80,
     }
-
+    try:
+        return f"Calories: {rfl[f]}"
+    except KeyError:
+        sys.exit()
 
 
 
