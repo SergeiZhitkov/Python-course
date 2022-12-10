824fafdc808b354c217bdb2a91cf9346929e0995
SergeiZhitkov <104830374+SergeiZhitkov@users.noreply.github.com>
2022-12-10 04:26:34 +0000

    Sat, Dec 10, 2022, 4:26 AM +00:00

---
+++ camel/camel.py
@@ -0,0 +1,16 @@
+def main():
+    camel_case = input("Your message in camelCase: ")
+    print(camel_to_snake(camel_case))
+
+
+
+
+def camel_to_snake(c):
+    for letter in c:
+        if letter.isalpha()
+
+
+
+
+if __name__ == "__main__":
+    main()
