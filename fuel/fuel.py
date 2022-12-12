7a74c4908020691a3a0f7de3b074836b680cbff6
SergeiZhitkov <104830374+SergeiZhitkov@users.noreply.github.com>
2022-12-12 04:44:13 +0000

    Mon, Dec 12, 2022, 4:44 AM +00:00

--- fuel/fuel.py
+++ fuel/fuel.py
@@ -1,10 +1,14 @@
 def main():
     while True:
         try:
-            fraction = input("Fraction: ")
-            
-
-
+            fraction = input("Fraction: ").strip()
+            x, y = fraction.split("/")
+            x = int(x)
+            y = int(y)
+            break
+        except:
+            pass
+    
 
 
 
