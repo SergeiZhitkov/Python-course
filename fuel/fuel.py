a7f45327fad95e9bfea13046344a8ab21a3c12ce
SergeiZhitkov <104830374+SergeiZhitkov@users.noreply.github.com>
2022-12-12 05:01:25 +0000

    Mon, Dec 12, 2022, 5:01 AM +00:00

--- fuel/fuel.py
+++ fuel/fuel.py
@@ -1,8 +1,12 @@
 def main():
     fraction = input("Fraction: ").strip()
-    percentage(fraction)
-    print(f"{fraction}%")
-
+    fraction = percentage(fraction)
+    if fraction >= 99:
+        print("F")
+    elif fraction <= 1:
+        print("E")
+    else:
+        print(f"{fraction:.0f}%")
 
 def percentage(f):
     while True:
@@ -11,7 +15,8 @@ def percentage(f):
             x = int(x)
             y = int(y)
             if x <= y and y != 0:
-                return float(x)/float(y) * 100
+                f = float(x) / float(y) * 100\
+                return float(x) / float(y) * 100
         except (ValueError, ZeroDivisionError):
             f = input("Fraction: ").strip()
             pass
