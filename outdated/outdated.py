8e25fe44b50c10d51d3d028bbde59fa551a99231
SergeiZhitkov <104830374+SergeiZhitkov@users.noreply.github.com>
2022-12-12 10:23:10 +0000

    Mon, Dec 12, 2022, 10:23 AM +00:00

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -15,17 +15,24 @@ months = [
     "december"
     ]
 while True:
-    date = input("Date: ").strip().lower()
-    if "/" in date:
-        try:
+    try:
+        date = input("Date: ").strip().lower()
+        if "/" in date:
             month, day, year = date.split("/")
             month = int(month)
             day = int(day)
-        except ValueError:
-            pass
-        if 0 < month < 13:
-            if 0 < day < 32:
-                print(f"{year}-{month:02}-{day:02}")
-                break
-    month, day, year = date.split(" ")
-    month
+            if 0 < month < 13:
+                if 0 < day < 32:
+                    print(f"{year}-{month:02}-{day:02}")
+                    break
+    except ValueError:
+        pass
+    try:
+        month, day, year = date.split(" ")
+        month = months[month]
+        day = day.replace(",", "")
+        day = int(day)
+        if 0 < day < 32:
+            print(f"{year}-{}")
+    except (ValueError, KeyError):
+        pass
