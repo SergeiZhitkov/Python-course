1451e1157102fd7fb5458eb4660b617988029697
SergeiZhitkov <104830374+SergeiZhitkov@users.noreply.github.com>
2022-12-14 12:27:23 +0000

    Wed, Dec 14, 2022, 12:27 PM +00:00

--- bitcoin/bitcoin.py
+++ bitcoin/bitcoin.py
@@ -1,5 +1,6 @@
 import requests
 import sys
+import json
 
 
 try:
@@ -10,5 +11,5 @@ except IndexError:
 except ValueError:
     sys.exit("Command-line argument is not a number")
 
-respond = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
-print(respond.json())
+response = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
+print(json.dumps(response.json(), indent=2))
