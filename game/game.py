621ce562fb965055c9bbea36297d90120314ae34
SergeiZhitkov <104830374+SergeiZhitkov@users.noreply.github.com>
2022-12-14 06:13:24 +0000

    Wed, Dec 14, 2022, 6:13 AM +00:00

--- game/game.py
+++ game/game.py
@@ -3,4 +3,12 @@ import random
 
 
 while True:
-    
+    try:
+        level = int(input("Level: "))
+        if level > 0:
+            break
+    except ValueError:
+        pass
+
+
+answer = random.rangint(1, leel)
