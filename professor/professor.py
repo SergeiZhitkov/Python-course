a4fefba1df231d8cd8cdc4e0db27c4b3556b3800
SergeiZhitkov <104830374+SergeiZhitkov@users.noreply.github.com>
2022-12-14 06:57:51 +0000

    Wed, Dec 14, 2022, 6:57 AM +00:00

--- professor/professor.py
+++ professor/professor.py
@@ -20,21 +20,25 @@ def generate_integer(level):
         raise ValueError
     if level == 1:
         for _ in range(10):
-            x = random.randint(1, 10)
-            y = random.randint(1, 10)
-            for _ in range(3):
-                try:
-                    answer = int(input(f"{x} + {y} = "))
-                except ValueError:
-                    print("EEE")
-                    pass
-                if answer == (x + y):
-                    score += 1
-                    break
-                else:
-                    print("EEE")
-                print(f"{x} + {y} = {x+y}")
-        print("Score:", score)
+            x = random.randint(1, 9)
+            y = random.randint(1, 9)
+    elif level == 2:
+        for _ in range(10):
+            x = random.randint(1, 99)
+            y = random.randint(1, 99)
+    for _ in range(3):
+        try:
+            answer = int(input(f"{x} + {y} = "))
+        except ValueError:
+            print("EEE")
+            pass
+        if answer == (x + y):
+            score += 1
+            break
+        else:
+            print("EEE")
+        print(f"{x} + {y} = {x+y}")
+    print("Score:", score)
 
 
 
