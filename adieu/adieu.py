418d015810981bf78a52817adee1d79eee242925
SergeiZhitkov <104830374+SergeiZhitkov@users.noreply.github.com>
2022-12-14 05:33:14 +0000

    Wed, Dec 14, 2022, 5:33 AM +00:00

--- adieu/adieu.py
+++ adieu/adieu.py
@@ -9,9 +9,13 @@ while True:
     try:
         name = input("Name: ")
     except EOFError:
-        print("\nAdieu, adieu, to ", end="")
-        for name in names[:-1]:
-            print(f"{name},", end="")
-        print(f"and {names[len(names)-1]}")
-        sys.exit()
+        if len(names) == 1:
+            print(f"\nAdieu, adieu, to {name}")
+            sys.exit()
+        else:
+            print("\nAdieu, adieu, to ", end="")
+            for name in names[:-1]:
+                print(f"{name},", end="")
+            print(f"and {names[len(names)-1]}")
+            sys.exit()
     names.append(name)
