46c38dc755874b77f8039ba3d5cccdc0a6bdb094
SergeiZhitkov <104830374+SergeiZhitkov@users.noreply.github.com>
2022-12-14 05:37:35 +0000

    Wed, Dec 14, 2022, 5:37 AM +00:00

--- adieu/adieu.py
+++ adieu/adieu.py
@@ -12,6 +12,9 @@ while True:
         if len(names) == 1:
             print(f"\nAdieu, adieu, to {name}")
             sys.exit()
+        elif len(names) == 2:
+            print(f"\nAdieu, adieu, to {names[0]} and {names[1]}")
+            sys.exit()
         else:
             print("\nAdieu, adieu, to ", end="")
             for name in names[:-1]:
