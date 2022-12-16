b2f6c1a5cf5231ef4d84c6fb1d28c77d0a98300d
SergeiZhitkov <104830374+SergeiZhitkov@users.noreply.github.com>
2022-12-16 10:14:11 +0000

    Fri, Dec 16, 2022, 10:14 AM +00:00

--- test_twttr/test_twttr.py
+++ test_twttr/test_twttr.py
@@ -1,7 +1,10 @@
 from twttr import shorten
 
-def test_word_with_vowels():
-    assert shorten("David") == "Dvd"
+def test_word_with_lower_vowels():
+    assert shorten("education") == "dctn"
+
+def test_word_with_upper_vowels():
+    assert shorten("EDUCATION") == "DCTN"
 
 
 def test_word_without_vowels():
