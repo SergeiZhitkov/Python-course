d68c6c9fd26604c5523597344a6a1763c2756501
SergeiZhitkov <104830374+SergeiZhitkov@users.noreply.github.com>
2022-12-16 10:10:56 +0000

    Fri, Dec 16, 2022, 10:10 AM +00:00

---
+++ test_twttr/twttr.py
@@ -0,0 +1,19 @@
+def main():
+    word = input("Enter your message: ").strip()
+    print(shorten(word))
+
+
+
+
+def shorten(word):
+    ov = ""
+    for letter  in word:
+        if letter not in "aeiouAEIOU":
+            ov = ov + letter
+    return ov
+
+
+
+
+if __name__ == "__main__":
+    main()
