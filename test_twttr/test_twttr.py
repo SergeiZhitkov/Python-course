98b2288913bd84963afa7a7094ac6ba02c055d69
SergeiZhitkov <104830374+SergeiZhitkov@users.noreply.github.com>
2022-12-16 10:26:49 +0000

    Fri, Dec 16, 2022, 10:26 AM +00:00

--- test_twttr/test_twttr.py
+++ test_twttr/test_twttr.py
@@ -6,8 +6,11 @@ def test_word_with_lower_vowels():
 def test_word_with_upper_vowels():
     assert shorten("EDUCATION") == "DCTN"
 
-
 def test_word_without_vowels():
     assert shorten("Gym") == "Gym"
-def test_numbers():
-    with pytest.raises("ValueError")
+
+def test_word_with_numbers():
+    assert shorten("Word123") == "Wrd123"
+
+def test_word_with_punctuations():
+    assert shorten("Hello, world") == "Hll, wrld"
