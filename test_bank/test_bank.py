45560f3d3d80fd8a7fe0921a8305dc39e8883346
SergeiZhitkov <104830374+SergeiZhitkov@users.noreply.github.com>
2022-12-16 10:40:20 +0000

    Fri, Dec 16, 2022, 10:40 AM +00:00

---
+++ test_bank/test_bank.py
@@ -0,0 +1,20 @@
+from bank import value
+
+
+def test_with_hello():
+    assert value("Hello, Jhon") == "$0"
+    assert value("hello, Jhon") == "$0"
+
+def test_with_h():
+    assert value("Hi, Mark") == "$20"
+    assert value("hi, Mark") == "$20"
+
+def test_without_first_h():
+    assert value("Good morning, Jhon") == "$100"
+    assert value("nice to meet you!") == "$100"
+
+def test_with_numbers():
+    assert value("01234Hello") == "$100"
+
+def test_with_punctuations():
+    assert value("__Hello, Jhon") == "$100"
