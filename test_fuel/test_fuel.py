803d91c10c18a669f4f2c01da6318d3290ea72b7
SergeiZhitkov <104830374+SergeiZhitkov@users.noreply.github.com>
2022-12-17 05:24:27 +0000

    Sat, Dec 17, 2022, 5:24 AM +00:00

--- test_fuel/test_fuel.py
+++ test_fuel/test_fuel.py
@@ -1,3 +1,16 @@
-from fuel import percentage
+import fuel
 
+def test_f_output():
+    assert gauge(99) == "F"
 
+def test_e_output():
+    assert gauge(1) == "E"
+
+def test_percentage_output():
+    assert gauge(14) == "14%"
+
+def test_convert():
+    assert convert("3/4") == 75
+
+def test_zero_convert():
+    assert conver
