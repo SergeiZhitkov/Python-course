10100b68e17b9eda80792dd6df10f885b3d14f45
SergeiZhitkov <104830374+SergeiZhitkov@users.noreply.github.com>
2022-12-18 11:48:18 +0000

    Sun, Dec 18, 2022, 11:48 AM +00:00

--- scourgify/scourgify.py
+++ scourgify/scourgify.py
@@ -1,6 +1,7 @@
 import csv
 import sys
 
+new_file = []
 
 if len(sys.argv) > 2:
     sys.exit("Too many command-line arguments")
@@ -9,3 +10,9 @@ elif len(sys.argv) < 2:
 
 if sys.argv[1].endswith(".csv") == False:
     sys.exit("Not a CSV file")
+
+try:
+    with open(sys.argv[1]) as file:
+        reader = csv.DictReader(file)
+        for name, house in reader:
+            new_file.append("name": name, "house": house)
