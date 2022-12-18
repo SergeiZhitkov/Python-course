116fe28765e911167003ecde31db07a5edc51198
SergeiZhitkov <104830374+SergeiZhitkov@users.noreply.github.com>
2022-12-18 10:24:25 +0000

    Sun, Dec 18, 2022, 10:24 AM +00:00

---
+++ lines/lines.py
@@ -0,0 +1,18 @@
+import sys
+
+if sys.argv > 2:
+    sys.exit("Too many command-line arguments")
+elif sys.argv < 2:
+    sys.exit("Too few command-line arguments")
+
+if sys.argv[1].endswith(".py") == False:
+    sys.exit("Not a Python file")
+
+with open(argv[1]) as file:
+    
+
+
+
+
+
+
