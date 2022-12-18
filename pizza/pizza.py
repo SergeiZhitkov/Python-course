51bc2d8751cc62dc46cd1e44f689206badcbdb72
SergeiZhitkov <104830374+SergeiZhitkov@users.noreply.github.com>
2022-12-18 11:04:07 +0000

    Sun, Dec 18, 2022, 11:04 AM +00:00

--- pizza/pizza.py
+++ pizza/pizza.py
@@ -1,6 +1,15 @@
 from tabulate import tabulate
 import sys
 
+if len(sys.argv) > 2:
+    sys.exit("Too many command-line arguments")
+elif len(sys.argv) < 2:
+    sys.exit("Too few command-line arguments")
 
+if sys.argv[1].endswith(".csv") == False:
+    sys.exit("Not a CSV file")
+
+try:
+    with open(sys.argv[1]) as file:
 
 
