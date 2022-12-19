176dc45d3b8b03e7098f7af1db2dc41298e61569
SergeiZhitkov <104830374+SergeiZhitkov@users.noreply.github.com>
2022-12-19 08:34:52 +0000

    Mon, Dec 19, 2022, 8:34 AM +00:00

--- shirt/shirt.py
+++ shirt/shirt.py
@@ -5,4 +5,7 @@ if len(sys.argv) > 3:
     sys.exit("Too many command-line arguments")
 elif len(sys.argv) < 3:
     sys.exit("Too few command-line arguments")
- 
+
+for file in sys.argv[1:]:
+    if file != file.endswith(".jpeg") and file != file.endswith(".png")
+        sys.exit("")
