3be620edc942af3bb204a4b45ac47364d0346fbf
SergeiZhitkov <104830374+SergeiZhitkov@users.noreply.github.com>
2022-12-19 08:47:13 +0000

    Mon, Dec 19, 2022, 8:47 AM +00:00

--- shirt/shirt.py
+++ shirt/shirt.py
@@ -2,17 +2,23 @@ import sys
 import csv
 import PIL
 
+extensions = [".jpeg", ".jpg", ".png"]
 if len(sys.argv) > 3:
     sys.exit("Too many command-line arguments")
 elif len(sys.argv) < 3:
     sys.exit("Too few command-line arguments")
 
+i=0
 for file in sys.argv[1:]:
-    if file != file.endswith(".jpeg") and file != file.endswith(".png")
-        sys.exit("")
+    for extension in extensions:
+        i += 1
+        if file.endswith(extension) == True:
+            break
+        if i == 3:
+            sys.exit("Invalid input")
 
 try:
     with Image.open(sys.argv[1]) as file:
         PIL.ImageOps.fit(file, method=Resampling.BICUBIC, bleed=0.0, centering=(0.5, 0.5))
         Image.paste(file, box=None, mask=None)
-        Image.save(fp, format=None, **params)
+        Image.save(sys.argv[2], format=None)
