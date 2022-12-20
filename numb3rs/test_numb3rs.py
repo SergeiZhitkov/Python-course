a3c333b1489ae5dbae0336535f17b4464ca977d5
SergeiZhitkov <104830374+SergeiZhitkov@users.noreply.github.com>
2022-12-20 10:41:11 +0000

    Tue, Dec 20, 2022, 10:41 AM +00:00

--- numb3rs/test_numb3rs.py
+++ numb3rs/test_numb3rs.py
@@ -1,14 +1,14 @@
 from numb3rs import validate
 
 def test_valid_data():
-    assert validate("255.255.255.255") == True
-    assert validate("99.99.99.99") == True
-    assert validate("0.0.0.0") == True
-    assert validate("255.94.5.0") == True
+    assert validate("255.255.255.255") == "True"
+    assert validate("99.99.99.99") == "True"
+    assert validate("0.0.0.0") == "True"
+    assert validate("255.94.5.0") == "True"
 
 def test_invalid_data():
-    assert validate("256.0.43.3") == False
-    assert validate("04.0.2.4")  == False
-    assert validate("012.3.24.2")  == False
-    assert validate("frg.0.3.4")  == False
-    assert validate("....")  == False
+    assert validate("256.0.43.3") == "False"
+    assert validate("04.0.2.4")  == "False"
+    assert validate("012.3.24.2")  == "False"
+    assert validate("frg.0.3.4")  == "False"
+    assert validate("....")  == "False"
