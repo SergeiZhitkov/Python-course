f2b31d39c2124acc7febc6fe15d79218d96d4421
SergeiZhitkov <104830374+SergeiZhitkov@users.noreply.github.com>
2022-12-20 10:44:08 +0000

    Tue, Dec 20, 2022, 10:44 AM +00:00

--- numb3rs/numb3rs.py
+++ numb3rs/numb3rs.py
@@ -8,9 +8,9 @@ def main():
 
 def validate(ip):
     if ip := re.search(r"^(1[0-9]{2}|2[0-5]{2}|2[0-4][0-9]|[1-9][0-9]|[0-9])(\.(1[0-9]{2}|2[0-5]{2}|2[0-4][0-9]|[1-9][0-9]|[0-9])){3}$", ip):
-        return "True"
+        return True
     else:
-        return "False"
+        return False
 
 
 
