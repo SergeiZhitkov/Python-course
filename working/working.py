cdcb4e48ca43dd374d85353f0914527a742b3a5a
SergeiZhitkov <104830374+SergeiZhitkov@users.noreply.github.com>
2022-12-21 10:34:32 +0000

    Wed, Dec 21, 2022, 10:34 AM +00:00

--- working/working.py
+++ working/working.py
@@ -7,7 +7,7 @@ def main():
 
 
 def convert(s):
-    if matches := re.search(r"^(1[0-2]|1[0-2]:[0-5][0-9]|[1-9]|[1-9]:[0-5][0-9]) (am|pm) to (1[0-2]|1[0-2]:[0-5][0-9]|[1-9]|[1-9]:[0-5][0-9])(am|pm)$", s, flags=re.IGNORECASE):
+    if matches := re.search(r"^(1[0-2]|1[0-2]:[0-5][0-9]|[1-9]|[1-9]:[0-5][0-9]) (am|pm) to (1[0-2]|1[0-2]:[0-5][0-9]|[1-9]|[1-9]:[0-5][0-9]) (am|pm)$", s.strip(), flags=re.IGNORECASE):
         if matches.group(2) == "am":
             
         matches.group(3)
