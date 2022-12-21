e683c2b431748b08076f3a057cf2723f28fdbe64
SergeiZhitkov <104830374+SergeiZhitkov@users.noreply.github.com>
2022-12-21 10:30:15 +0000

    Wed, Dec 21, 2022, 10:30 AM +00:00

--- working/working.py
+++ working/working.py
@@ -7,7 +7,7 @@ def main():
 
 
 def convert(s):
-    if s := re.search(r"(1-9]) (am|pm)? to ()(am|pm)?$", s, flags=re.IGNORECASE):
+    if s := re.search(r"^(1[0-2]|1[0-2]:[0-5][0-9]|[1-9]|) (am|pm) to (1[0-2]|[1-9])(am|pm)$", s, flags=re.IGNORECASE):
         return
     else:
         raise ValueError
