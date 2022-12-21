dc4a86b404775031daf88ca52339be291acf7d4a
SergeiZhitkov <104830374+SergeiZhitkov@users.noreply.github.com>
2022-12-21 10:36:23 +0000

    Wed, Dec 21, 2022, 10:36 AM +00:00

--- working/working.py
+++ working/working.py
@@ -1,6 +1,14 @@
 import re
 import sys
 
+time = {
+    "am" : {
+        12
+    }
+
+
+}
+
 
 def main():
     print(convert(input("Hours: ")))
@@ -9,7 +17,7 @@ def main():
 def convert(s):
     if matches := re.search(r"^(1[0-2]|1[0-2]:[0-5][0-9]|[1-9]|[1-9]:[0-5][0-9]) (am|pm) to (1[0-2]|1[0-2]:[0-5][0-9]|[1-9]|[1-9]:[0-5][0-9]) (am|pm)$", s.strip(), flags=re.IGNORECASE):
         if matches.group(2) == "am":
-            
+
         matches.group(3)
         matches.group(4)
 
