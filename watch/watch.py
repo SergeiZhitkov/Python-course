0b7041b3ade7c96565526a48cc53dcf18957e310
SergeiZhitkov <104830374+SergeiZhitkov@users.noreply.github.com>
2022-12-21 04:51:06 +0000

    Wed, Dec 21, 2022, 4:51 AM +00:00

--- watch/watch.py
+++ watch/watch.py
@@ -7,7 +7,7 @@ def main():
 
 
 def parse(s):
-    if s := re.search(r'"https?(?:www\.)?://youtube\.com/embed/(\w)"$', s, flag=re.IGNORECASE):
+    if s := re.search(r'"https?(?:www\.)?://youtube\.com/embed/(\w)"$', s, flags=re.IGNORECASE):
         return s
     else:
         return None
