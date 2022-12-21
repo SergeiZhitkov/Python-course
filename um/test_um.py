2bfa286f9d53fe6d9f48999c4c9472704032a2e6
SergeiZhitkov <104830374+SergeiZhitkov@users.noreply.github.com>
2022-12-21 11:44:52 +0000

    Wed, Dec 21, 2022, 11:44 AM +00:00

--- um/test_um.py
+++ um/test_um.py
@@ -3,8 +3,8 @@ import pytest
 
 def test_um():
     assert count("hello, um") == 1
+    assert count("copium") == 0
     assert count("um, hello, ok, um") == 2
     assert count("hello,um,ok,um") == 2
     assert count("copium") == 0
     assert count("ferg") == 0
-    
