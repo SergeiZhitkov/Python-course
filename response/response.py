80171d1e85bc4fc43ad11fbbef81aa79c1f46f17
SergeiZhitkov <104830374+SergeiZhitkov@users.noreply.github.com>
2022-12-22 09:03:57 +0000

    Thu, Dec 22, 2022, 9:03 AM +00:00

--- response/response.py
+++ response/response.py
@@ -1,9 +1,7 @@
 from validator_collection import validators
-import sys
 
 email_address = input("What's your email address? ").strip().lower()
-try:
-    email_address = validators.email(email_address)
+if email_address := validators.email(email_address):
     print("Valid")
-except ValueError:
-    sys.exit("Invalid")
+else:
+    print("Invalid")
