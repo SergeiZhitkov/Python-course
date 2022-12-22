475c2279685d958e2300deded9e38bb109c521ca
SergeiZhitkov <104830374+SergeiZhitkov@users.noreply.github.com>
2022-12-22 08:57:04 +0000

    Thu, Dec 22, 2022, 8:57 AM +00:00

--- response/response.py
+++ response/response.py
@@ -1,3 +1,10 @@
 import validator_collection
 
-input("Email: ").is_email
+email_address = input("What's your email address? ").strip().lower()
+try:
+    email_address = validators.email(None)
+    # Will raise an EmptyValueError
+except errors.EmptyValueError:
+    # Handling logic goes here
+except errors.InvalidEmailError:
+    # More handlign logic goes here
