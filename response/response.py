4c89cea85b9c0857939012f3cd41da912a822eda
SergeiZhitkov <104830374+SergeiZhitkov@users.noreply.github.com>
2022-12-22 08:59:42 +0000

    Thu, Dec 22, 2022, 8:59 AM +00:00

--- response/response.py
+++ response/response.py
@@ -1,10 +1,8 @@
-import validator_collection
+from validator_collection import validators
+import sys
 
 email_address = input("What's your email address? ").strip().lower()
 try:
-    email_address = validators.email(None)
-    # Will raise an EmptyValueError
-except errors.EmptyValueError:
-    # Handling logic goes here
-except errors.InvalidEmailError:
-    # More handlign logic goes here
+    email_address = validators.email(email_address)
+except ValueError:
+    sys.exit("Invalid")
