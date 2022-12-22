553f7905614aabf75d98fdea82b738b0730c0cf1
SergeiZhitkov <104830374+SergeiZhitkov@users.noreply.github.com>
2022-12-22 09:00:40 +0000

    Thu, Dec 22, 2022, 9:00 AM +00:00

--- response/response.py
+++ response/response.py
@@ -4,5 +4,6 @@ import sys
 email_address = input("What's your email address? ").strip().lower()
 try:
     email_address = validators.email(email_address)
+    print("Valid")
 except ValueError:
     sys.exit("Invalid")
