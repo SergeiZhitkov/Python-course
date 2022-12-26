b93029e3d63001176ccd4c0f8de0fcc5057d93e8
SergeiZhitkov <104830374+SergeiZhitkov@users.noreply.github.com>
2022-12-26 10:32:29 +0000

    Mon, Dec 26, 2022, 10:32 AM +00:00

--- seasons/test_seasons.py
+++ seasons/test_seasons.py
@@ -2,7 +2,7 @@ import pytest
 from seasons import how_many_minutes
 def test_valid_date():
     assert how_many_minutes("2001-09-03") == "Eleven million, two hundred eight thousand, nine hundred sixty minutes"
-    assert how_many_minutes("2022-12-26") == "zero minutes"
+    assert how_many_minutes("2022-12-26") == "Zero minutes"
 
 def test_invalid_date():
     with pytest.raises(SystemExit):
