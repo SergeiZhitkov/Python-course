dba98342ab07a82e745f98177e80afa91d84df19
SergeiZhitkov <104830374+SergeiZhitkov@users.noreply.github.com>
2022-12-26 08:59:43 +0000

    Mon, Dec 26, 2022, 8:59 AM +00:00

---
+++ seasons/seasons.py
@@ -0,0 +1,16 @@
+from datetime import date
+import sys
+
+def main():
+    print(how_many_minutes(input("Date of Birth: ")))
+
+
+
+def how_many_minutes(s):
+    
+
+
+
+
+if __name__ == "__main__":
+    main()
