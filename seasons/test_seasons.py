ccdb1ec72c487e8b9b3b784240dc58dc7f74b66a
SergeiZhitkov <104830374+SergeiZhitkov@users.noreply.github.com>
2022-12-26 10:10:07 +0000

    Mon, Dec 26, 2022, 10:10 AM +00:00

--- seasons/test_seasons.py
+++ seasons/test_seasons.py
@@ -5,13 +5,13 @@ def test_valid_date():
     assert how_many_minutes("2022-12-26") == "zero"
 
 def test_invalid_date():
-    with pytest.raises(ValueError):
+    with pytest.raises(SystemExit):
         how_many_minutes("2023.09.03")
-    with pytest.raises(ValueError):
+    with pytest.raises(SystemExit):
         how_many_minutes("bgnthm")
-    with pytest.raises(ValueError):
+    with pytest.raises(SystemExit):
         how_many_minutes("2022-12-54")
-    with pytest.raises(ValueError):
+    with pytest.raises(SystemExit):
         how_many_minutes("2022-43-05")
-    with pytest.raises(ValueError):
+    with pytest.raises(SystemExit):
         how_many_minutes("100000-02-03")
