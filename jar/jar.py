47b1246553c790f2cfe0d333ce77c4554c8e596d
SergeiZhitkov <104830374+SergeiZhitkov@users.noreply.github.com>
2022-12-27 04:42:24 +0000

    Tue, Dec 27, 2022, 4:42 AM +00:00

--- jar/jar.py
+++ jar/jar.py
@@ -1,3 +1,19 @@
 class Jar:
-    def __init__(self):
-        
+    def __init__(self, capasity):
+        if capasity 
+        self.capasity = capasity
+
+
+
+
+def main():
+    self.capasity = input("Capasity of jar: ")
+
+
+
+
+
+
+
+if "__name__" == "__main__":
+    main()
