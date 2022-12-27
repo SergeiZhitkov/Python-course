7382832b5a2f424584c9bb9f53a521c81cb0459f
SergeiZhitkov <104830374+SergeiZhitkov@users.noreply.github.com>
2022-12-27 04:47:31 +0000

    Tue, Dec 27, 2022, 4:47 AM +00:00

--- jar/jar.py
+++ jar/jar.py
@@ -1,13 +1,16 @@
 class Jar:
-    def __init__(self, capasity):
-        if capasity 
+    def __init__(self, capasity, cookies):
+        if capasity != int(capasity) or capasity < 0:
+            raise ValueError("Invalid input")
         self.capasity = capasity
+        self.cookies = cookies
 
-
-
+    def __str__(self, cookies)
 
 def main():
-    self.capasity = input("Capasity of jar: ")
+    try:
+        self.capasity = input("Capasity of jar: ")
+    except ValueError:
 
 
 
