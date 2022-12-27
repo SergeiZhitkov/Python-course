acf81fa8573b625e29748254e493697536bd8929
SergeiZhitkov <104830374+SergeiZhitkov@users.noreply.github.com>
2022-12-27 04:58:00 +0000

    Tue, Dec 27, 2022, 4:58 AM +00:00

--- jar/jar.py
+++ jar/jar.py
@@ -1,18 +1,23 @@
 class Jar:
-    def __init__(self, capasity, deposit, withdraw):
-        if capasity != int(capasity) or capasity < 0:
-            raise ValueError("Invalid input")
-        if deposit != int(deposit) or capasity < deposit or deposit < 0:
-            raise ValueError("Invalid input")
-        if withdraw != int(withdraw) or withdraw > deposit or withdraw < 0:
-            raise ValueError("Invalid input")
-        self.capasity = capasity
-        self.deposit = deposit
-        self.withdraw = withdraw
-
+    def __init__(self, capacity=12):
+        self.capacity = capasity
 
     def __str__(self):
-        return "🍪" * self.deposit
+        ...
+
+    def deposit(self, n):
+        ...
+
+    def withdraw(self, n):
+        ...
+
+    @property
+    def capacity(self):
+        ...
+
+    @property
+    def size(self):
+
 
 def main():
     try:
